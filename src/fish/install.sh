5d76fc17a68fcb6762fea1bbca3b906fafbc40db
Guillaume CAMUS <guillaume.camus@gmail.com>
2022-09-16 19:59:04 +0200

    fix(fish): install fish plugin for non root

--- src/fish/install.sh
+++ src/fish/install.sh
@@ -2,15 +2,33 @@
 
 echo "Activating fist ..."
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
 PURE=${PURE:-true}
+USER=${2:-"automatic"}
+
+# Determine the appropriate non-root user
+if [ "${USER}" = "auto" ] || [ "${USER}" = "automatic" ]; then
+    USER=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" > /dev/null 2>&1; then
+            USER=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USER}" = "" ]; then
+        USER=root
+    fi
+elif [ "${USER}" = "none" ] || ! id -u ${USER} > /dev/null 2>&1; then
+    USER=root
+fi
 
 echo "Installing fish shell..."
-if grep -q 'Ubuntu' < /etc/os-release; then
-    check_packages software-properties-common
-    apt-add-repository -y ppa:fish-shell/release-3
-    apt-get update
-    apt-get -y install --no-install-recommends fish
-elif grep -q 'Debian' < /etc/os-release; then
+if grep -q 'Debian' < /etc/os-release; then
     if grep -q 'stretch' < /etc/os-release; then
         echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_9.0/ /' | tee /etc/apt/sources.list.d/shells:fish:release:3.list
         curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_9.0/Release.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
