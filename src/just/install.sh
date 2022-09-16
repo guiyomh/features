390bf7261736baed3bbbe32893a87a9338cb8b29
Guillaume CAMUS <guillaume.camus@gmail.com>
2022-09-16 20:26:53 +0200

    feat(just): add just a command runner

---
+++ src/just/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Activating vim ..."
+
+PURE=${PURE:-true}
+
+echo "Installing just ..."
+curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+apt update
+
+sudo apt install just
+just --version
+
+echo "Done!"
