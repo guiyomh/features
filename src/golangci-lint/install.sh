4ad751d7e7ed20fcf7c8febf2f8a8b1f5d13972c
Guillaume CAMUS <guillaume.camus@gmail.com>
2022-11-04 18:30:45 +0000

    feat(golangci-lint): allow install on multi-os

--- src/golangci-lint/install.sh
+++ src/golangci-lint/install.sh
@@ -7,8 +7,76 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-VERSION=${VERSION:-"latest"}
+GOLANGCI_VERSION=${VERSION:-"latest"}
 USER=${2:-"automatic"}
+
+export DEBIAN_FRONTEND=noninteractive
+
+# shellcheck source=/dev/null
+source /etc/os-release
+
+apt_get_update() {
+  case "${ID}" in
+    debian|ubuntu)
+      if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+      fi
+    ;;
+  esac
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+  case "${ID}" in
+    debian|ubuntu)
+      if ! dpkg -s "$@" >/dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+      fi
+    ;;
+    alpine)
+      if ! apk -e info "$@" >/dev/null 2>&1; then
+        apk add --no-cache "$@"
+      fi
+    ;;
+  esac
+}
+
+find_version_from_git_tags() {
+    local variable_name=$1
+    local requested_version=${!variable_name}
+    if [ "${requested_version}" = "none" ]; then return; fi
+    local repository=$2
+    local prefix=${3:-"tags/v"}
+    local separator=${4:-"."}
+    local last_part_optional=${5:-"false"}
+    if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
+        local escaped_separator=${separator//./\\.}
+        local last_part
+        if [ "${last_part_optional}" = "true" ]; then
+            last_part="(${escaped_separator}[0-9]+)*?"
+        else
+            last_part="${escaped_separator}[0-9]+"
+        fi
+        local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
+        local version_list
+        check_packages git ca-certificates
+        version_list="$(git ls-remote --tags "${repository}" | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
+        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
+            declare -g "${variable_name}"="$(echo "${version_list}" | head -n 1)"
+        else
+            set +e
+            declare -g "${variable_name}"="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            set -e
+        fi
+    fi
+    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
+        echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
+        exit 1
+    fi
+    echo "${variable_name}=${!variable_name}"
+}
 
 # Determine the appropriate non-root user
 if [ "${USER}" = "auto" ] || [ "${USER}" = "automatic" ]; then
@@ -28,11 +96,26 @@ elif [ "${USER}" = "none" ] || ! id -u ${USER} > /dev/null 2>&1; then
 fi
 
 echo "Installing golangci-lint ..."
-if [ "${USER}" != "root" ]; then
-    sudo -u "$USER" -c "go install github.com/golangci/golangci-lint/cmd/golangci-lint@${VERSION}"
-else
-    go install github.com/golangci/golangci-lint/cmd/golangci-lint@"${VERSION}"
-fi
+
+# Install dependencies if missing
+check_packages curl ca-certificates
+
+# Soft version matching
+find_version_from_git_tags GOLANGCI_VERSION "https://github.com/golangci/golangci-lint" "tags/v" "." "true"
+
+case "${ID}" in
+    debian|ubuntu)
+        curl -sL -o /tmp/golangci-lint.deb "https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_VERSION}/golangci-lint-${GOLANGCI_VERSION}-linux-amd64.deb"
+        dpkg -i /tmp/golangci-lint.deb
+        rm -rf /tmp/golangci-lint.deb
+    ;;
+    alpine)
+        curl -sL -o /tmp/golangci-lint.tar.gz "https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_VERSION}/golangci-lint-${GOLANGCI_VERSION}-linux-arm64.tar.gz"
+        tar xvf /tmp/golangci-lint.tar.gz "golangci-lint-${GOLANGCI_VERSION}-linux-arm64/golangci-lint"
+        mv "golangci-lint-${GOLANGCI_VERSION}-linux-arm64/golangci-lint" /usr/local/bin/golangci-lint
+        rm -rf  /tmp/golangci-lint.tar.gz "golangci-lint-${GOLANGCI_VERSION}-linux-arm64/"
+    ;;
+esac
 
 golangci-lint --version
 
