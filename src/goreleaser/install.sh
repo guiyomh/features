1600cde7be083ff7e5fc206b15e93d18220fa9ab
Guillaume CAMUS <guillaume.camus@gmail.com>
2022-11-04 17:58:46 +0000

    feat(gorelease): allow install on multi-os

--- src/goreleaser/install.sh
+++ src/goreleaser/install.sh
@@ -7,8 +7,76 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-VERSION=${VERSION:-"latest"}
+GORELEASER_VERSION=${VERSION:-"latest"}
 USER=${2:-"automatic"}
+
+export DEBIAN_FRONTEND=noninteractive
+
+# shellcheck source=/dev/null
+source /etc/os-release
+
+apt_get_update() {
+  case "${ID}" in
+    debian|ubuntu)
+      if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+      fi
+    ;;
+  esac
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+  case "${ID}" in
+    debian|ubuntu)
+      if ! dpkg -s "$@" >/dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+      fi
+    ;;
+    alpine)
+      if ! apk -e info "$@" >/dev/null 2>&1; then
+        apk add --no-cache "$@"
+      fi
+    ;;
+  esac
+}
+
+find_version_from_git_tags() {
+    local variable_name=$1
+    local requested_version=${!variable_name}
+    if [ "${requested_version}" = "none" ]; then return; fi
+    local repository=$2
+    local prefix=${3:-"tags/v"}
+    local separator=${4:-"."}
+    local last_part_optional=${5:-"false"}
+    if [ "$(echo "${requested_version}" | grep -o "." | wc -l)" != "2" ]; then
+        local escaped_separator=${separator//./\\.}
+        local last_part
+        if [ "${last_part_optional}" = "true" ]; then
+            last_part="(${escaped_separator}[0-9]+)*?"
+        else
+            last_part="${escaped_separator}[0-9]+"
+        fi
+        local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
+        local version_list
+        check_packages git ca-certificates
+        version_list="$(git ls-remote --tags "${repository}" | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
+        if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
+            declare -g "${variable_name}"="$(echo "${version_list}" | head -n 1)"
+        else
+            set +e
+            declare -g "${variable_name}"="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            set -e
+        fi
+    fi
+    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
+        echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
+        exit 1
+    fi
+    echo "${variable_name}=${!variable_name}"
+}
 
 # Determine the appropriate non-root user
 if [ "${USER}" = "auto" ] || [ "${USER}" = "automatic" ]; then
@@ -29,11 +97,26 @@ fi
 
 echo "Installing goreleaser ..."
 
-if [ "${USER}" != "root" ]; then
-    sudo -u "$USER" -c "go install github.com/goreleaser/goreleaser@${VERSION}"
-else
-    go install github.com/goreleaser/goreleaser@"${VERSION}"
-fi
+# Install dependencies if missing
+check_packages curl ca-certificates
+
+# Soft version matching
+find_version_from_git_tags GORELEASER_VERSION "https://github.com/goreleaser/goreleaser" "tags/v" "." "true"
+
+case "${ID}" in
+    debian|ubuntu)
+        echo "https://github.com/goreleaser/goreleaser/releases/download/v${GORELEASER_VERSION}/goreleaser_${GORELEASER_VERSION}_amd64.deb" > /tmp/url.txt
+        curl -sL -o /tmp/goreleaser.deb "https://github.com/goreleaser/goreleaser/releases/download/v${GORELEASER_VERSION}/goreleaser_${GORELEASER_VERSION}_amd64.deb"
+        dpkg -i /tmp/goreleaser.deb
+        rm -rf /tmp/goreleaser.deb
+    ;;
+    alpine)
+        echo "https://github.com/goreleaser/goreleaser/releases/download/v${GORELEASER_VERSION}/goreleaser_${GORELEASER_VERSION}_aarch64.apk" > /tmp/url.txt
+        curl -sL -o /tmp/goreleaser.apk "https://github.com/goreleaser/goreleaser/releases/download/v${GORELEASER_VERSION}/goreleaser_${GORELEASER_VERSION}_aarch64.apk"
+        apk add --allow-untrusted /tmp/goreleaser.apk
+        rm -rf /tmp/goreleaser.apk
+    ;;
+esac
 
 goreleaser --version
 
